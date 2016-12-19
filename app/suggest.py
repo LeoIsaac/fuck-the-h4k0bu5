ea60e5ebd24945a471b9902873d0498f5fe11e36
Isaac <r.sato7690@me.com>
2016-12-19 14:36:05 +0900

    Modify suggest

--- app/suggest.py
+++ app/suggest.py
@@ -1,14 +1,25 @@
+from urllib.parse import urlencode
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 class Suggest:
     def getSuggest(self, ori:str, des:str) -> dict:
-        if not ori or not des: return {}
+        if not ori and not des: return {}
 
-        json = {}
+        params = urlencode({'stopname_f': ori, 'stopname_t': des}, encoding='sjis')
+        html = urlopen('http://www.hakobus.jp/search02.php?' + params)
+        soup = BeautifulSoup(html, "html.parser")
+
         origin = []
         destination = []
-        origin.append({"stop": ori})
-        destination.append({"stop": des})
+
+        for stop in soup.find('select', id="in").find_all('option'):
+            origin.append({"stop": stop.string})
+
+        for stop in soup.find('select', id="out").find_all('option'):
+            destination.append({"stop": stop.string})
+
+        json = {}
         json["origin"] = origin
         json["destination"] = destination
         return json
